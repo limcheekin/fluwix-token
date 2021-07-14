9d220d6a8532c4b44f223db96f378cec84e1b2f5
Lim Chee Kin <limcheekin@vobject.com>
2021-07-14 10:38:52 +0800

    chore: updated configs and dependencies for contract deployment to rinkeby testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var FluwixToken = artifacts.require("./FluwixToken.sol");
 
 module.exports = function (deployer, network, accounts) {
+  console.log('Accounts:\n', accounts);
   const initialAccount = accounts[0]
   const initialBalance = '1000000000'
   const name = 'FluwixToken'
--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +11,21 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+
+    // Useful for deploying to a public network.
+    // It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://rinkeby.infura.io/v3/59427091d14941beabc41caadcf2b6f9`
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000,
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   }
 };
